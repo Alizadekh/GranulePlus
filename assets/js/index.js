7ffd401d213e6b7b6e5eab0f4e2055529294e7b0
Alizadekh <aykhanalizadekh@gmail.com>
2024-07-27 15:46:10 +0400

    Completed Main Page

---
+++ assets/js/index.js
@@ -0,0 +1,52 @@
+const searchBtn = document.querySelector(".searchBtn");
+const searchInput = document.querySelector("#searchInput");
+
+let isOpen = false;
+
+function toggleSearch() {
+  if (searchInput.value.length > 0) {
+    searchInput.classList.add("open");
+    searchInput.classList.remove("closed");
+  } else if (isOpen) {
+    searchInput.classList.add("closed");
+    searchInput.classList.remove("open");
+  } else {
+    searchInput.classList.add("open");
+    searchInput.classList.remove("closed");
+  }
+  isOpen = !isOpen;
+}
+
+searchBtn.addEventListener("click", toggleSearch);
+
+// Swiper
+
+var swiper = new Swiper(".scrollswiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
+
+// Navbar Open Mobile
+const navBtn = document.querySelector("#navOpenClose");
+const navigation = document.querySelector("#navigation");
+let navOpen = false;
+
+navBtn.addEventListener("click", function () {
+  if (navOpen) {
+    navigation.style.display = "none";
+  } else {
+    navigation.style.display = "block";
+  }
+  navOpen = !navOpen;
+});
