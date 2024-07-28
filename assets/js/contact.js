c4c9a49fd74ed93143b99d87f34cb3d61f3ca3dd
Alizadekh <aykhanalizadekh@gmail.com>
2024-07-28 12:25:43 +0400

    Create Contact

---
+++ assets/js/contact.js
@@ -0,0 +1,44 @@
+function sendEmail() {
+  var email = document.getElementById("email").value;
+  var fullname = document.getElementById("fullname").value;
+  var phone = document.getElementById("phone").value;
+  var message = document.getElementById("message").value;
+
+  var mailtoLink = `mailto:aykhanalizadekh@gmail.com?subject=Contact Form Submission&body=Name: ${fullname}%0D%0AEmail: ${email}%0D%0APhone: ${phone}%0D%0AMessage: ${message}`;
+
+  window.location.href = mailtoLink;
+}
+
+const searchBtn = document.querySelector(".searchBtn");
+const searchInput = document.querySelector("#searchInput");
+
+let isOpen = false;
+
+function toggleSearch() {
+  if (searchInput.value.length > 0) {
+    searchInput.classList.add("open");
+    searchInput.classList.remove("closed");
+  } else if (isOpen) {
+    searchInput.classList.add("closed");
+    searchInput.classList.remove("open");
+  } else {
+    searchInput.classList.add("open");
+    searchInput.classList.remove("closed");
+  }
+  isOpen = !isOpen;
+}
+searchBtn.addEventListener("click", toggleSearch);
+
+// Navbar Open Mobile
+const navBtn = document.querySelector("#navOpenClose");
+const navigation = document.querySelector("#navigation");
+let navOpen = false;
+
+navBtn.addEventListener("click", function () {
+  if (navOpen) {
+    navigation.style.display = "none";
+  } else {
+    navigation.style.display = "block";
+  }
+  navOpen = !navOpen;
+});
